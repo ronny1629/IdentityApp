2b265d7193f3d44e88908e5995b9e96186003734
ronny1629 <ronnymarte1629@gmail.com>
2024-04-13 02:29:35 -0400

    Nuevo cambio

---
+++ IdentityApp/Controllers/AccountController.cs
@@ -0,0 +1,65 @@
+﻿using IdentityApp.DTOs.Account;
+using IdentityApp.Models;
+using IdentityApp.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace IdentityApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AccountController : ControllerBase
+    {
+        private readonly JWTService _jwtService;
+        private readonly SignInManager<User> _signInManager;
+        private readonly UserManager<User> _userManager;
+
+        public AccountController(JWTService jwtService, SignInManager<User> signInManager, UserManager<User> userManager)
+        {
+            _jwtService = jwtService;
+            _signInManager = signInManager;
+            _userManager = userManager;
+        }
+
+        [HttpPost("login")]
+        public async Task<ActionResult<UserDto>> Login(LoginDto model)
+        {
+            var user = await _userManager.FindByNameAsync(model.Username);
+
+            if (user == null)
+            {
+                return Unauthorized("Invalid username or password");
+            }
+
+            if (user.EmailConfirmed == false) 
+            {
+                return Unauthorized("Please confirm your email.");
+            }
+
+            var result = await _signInManager.CheckPasswordSignInAsync(user, model.Password, false);
+            if (!result.Succeeded)
+            {
+                return Unauthorized("Invalid UserName or password");
+            }
+
+            return CreateApplicationUserDto(user);
+        }
+
+
+ #region private Helper Methods
+        private UserDto CreateApplicationUserDto(User user)
+        {
+            return new UserDto
+            {
+                FirstName = user.FirstName,
+                LastName = user.UserName,
+                JWT = _jwtService.CreateJWT(user),
+
+            };
+        }
+ #endregion
+
+    }
+}
---
+++ IdentityApp/Services/JWTService.cs
@@ -0,0 +1,49 @@
+﻿using IdentityApp.Models;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace IdentityApp.Services
+{
+    public class JWTService
+    {
+        private readonly IConfiguration _configuration;
+        private readonly SymmetricSecurityKey _jwtKey;
+
+        public JWTService(IConfiguration configuration)
+        {
+            _configuration = configuration;
+            //jwtKey is used for both encripting and descripting the JWT token
+            _jwtKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["JWT:Key"]));
+        }
+        public string CreateJWT(User user)
+        {
+            var userClaims = new List<Claim>
+            {
+                new Claim(ClaimTypes.NameIdentifier, user.Id),
+                new Claim(ClaimTypes.Email, user.Email),
+                new Claim(ClaimTypes.GivenName, user.FirstName),
+                new Claim(ClaimTypes.Surname, user.LastName),
+                new Claim("my own claim name", "this is the value")
+            };
+
+            var credentials = new SigningCredentials(_jwtKey, SecurityAlgorithms.HmacSha512Signature);
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(userClaims),
+                Expires = DateTime.UtcNow.AddDays(int.Parse(_configuration["JWT:ExpiresInDay"])),
+                SigningCredentials = credentials,
+                Issuer = _configuration["JWT:Issuer"]
+            };
+
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var jwt = tokenHandler.CreateToken(tokenDescriptor);
+            return tokenHandler.WriteToken(jwt);
+        }
+    }
+}
